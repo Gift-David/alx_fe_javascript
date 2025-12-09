5fb040ca462ad8eb17fb7fcb6f84f3f83c5db1f2
David Gift <giftdavid189@gmail.com>
2025-12-09 16:53:34 +0100

    added DOM tasks

---
+++ dom-manipulation/script.js
@@ -0,0 +1,15 @@
+const newQuote = document.getElementById('newQuote')
+const quoteDisplay = document.getElementById('quoteDisplay')
+
+const quote = []
+
+function showRandomQuote(params) {
+    
+}
+
+function createAddQuoteForm(params) {
+    
+}
+
+showRandomQuote()
+createAddQuoteForm()
